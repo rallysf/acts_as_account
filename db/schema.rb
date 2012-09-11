81f80a4e9e42086e2c4743184dcee8c95ab14859
Andrew Hubbs & John Brandy <pair+andrew+john@rally.org>
2012-09-11 16:13:58 -0700

    update .rvmrc and format schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -4,16 +4,16 @@ ActiveRecord::Schema.define(:version => 1) do
     execute "ALTER TABLE acts_as_account_postings DROP FOREIGN KEY other_account_id"
     execute "ALTER TABLE acts_as_account_postings DROP FOREIGN KEY journal_id"
   end
-  
+
   create_table "acts_as_account_accounts", :force => true do |t|
-    t.integer  "holder_id", :null => false
-    t.string   "holder_type", :null => false
-    t.string   "name", :null => false
-    
-    t.integer  "balance", :default => 0
-    t.integer  "postings_count", :default => 0
-    t.datetime  "last_valuta"
-    
+    t.integer "holder_id", :null => false
+    t.string "holder_type", :null => false
+    t.string "name", :null => false
+
+    t.integer "balance", :default => 0
+    t.integer "postings_count", :default => 0
+    t.datetime "last_valuta"
+
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(:version => 1) do
     t.integer "other_account_id", :null => false
     t.integer "journal_id", :null => false
     t.integer "amount", :null => false
-    
+
     t.integer "reference_id"
     t.string "reference_type"
 
@@ -42,21 +42,21 @@ ActiveRecord::Schema.define(:version => 1) do
   add_index "acts_as_account_postings", "journal_id"
   add_index "acts_as_account_postings", ["reference_type", "reference_id"], :name => "reference"
   add_index "acts_as_account_postings", ["valuta", "id"], :name => "sort_key"
-  
+
   execute "ALTER TABLE acts_as_account_postings ADD CONSTRAINT account_id FOREIGN KEY (account_id) REFERENCES acts_as_account_accounts (id)"
   execute "ALTER TABLE acts_as_account_postings ADD CONSTRAINT other_account_id FOREIGN KEY (other_account_id) REFERENCES acts_as_account_accounts (id)"
   execute "ALTER TABLE acts_as_account_postings ADD CONSTRAINT journal_id FOREIGN KEY (journal_id) REFERENCES acts_as_account_journals (id)"
 
   create_table "acts_as_account_global_accounts", :force => true do |t|
-    t.string   "name", :null => false
+    t.string "name", :null => false
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name", :null => false
+    t.string "name", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-  
+
   create_table "cheques", :force => true do |t|
     t.string "number"
     t.datetime "created_at"
